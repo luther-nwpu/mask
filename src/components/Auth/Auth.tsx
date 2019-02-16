5438b91ee4d033658164d3614808e20c82f20917
e <luthernpu@gmail.com>
2019-02-16 17:17:48 +0800

    d

---
+++ src/components/Auth/Auth.tsx
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+import * as React from 'react'
+import './Auth.scss'
+export class Auth extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    public render() {
+        return (
+            <div className="auth-component">
+                <div className="modal">
+
+                </div>
+            </div>
+        )
+    }
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,13 @@
 import './Home.scss'
 import React from 'react'
-import { TopBar } from '@components'
+import { TopBar, Auth } from '@components'
 
 export class Home extends React.Component {
   render () {
     return (
       <div className="mask-app">
           <TopBar></TopBar>
+          <Auth></Auth>
       </div>
     )
   }
