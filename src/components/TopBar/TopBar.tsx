05f651ec1996d2730a5079351d00c5bb3762db54
e <luthernpu@gmail.com>
2019-02-16 10:44:31 +0800

    d

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -8,39 +8,42 @@ export class TopBar extends Component {
         return (
             <div className="topbar">
                 <div className="topbar-content">
-
-                    <a href ="/"> <img src={logo} /> </a>
-                    <div className="item">
-                        首页
-                    </div>
-                    <div className="item">
-                        <div> 分类 </div>
-                        <span className="closeBox" >
-                        </span>
-                    </div>
-                    <div className="item">
-                        热门
-                        <span className="closeBox" >
-                        </span>
-                    </div>
-                    <div className="item">
-                        排行榜
-                        <span className="closeBox">
-                        </span>
-                    </div>
-                    <div className="input">
-                        <input type = "text" />
-                        <div className="icon">
+                    <div className="left">
+                        <a href ="/"> <img src={logo} /> </a>
+                        <div className="item">
+                            首页
+                        </div>
+                        <div className="item">
+                            <div> 分类 </div>
+                            <span className="closeBox" >
+                            </span>
+                        </div>
+                        <div className="item">
+                            热门
+                            <span className="closeBox" >
+                            </span>
+                        </div>
+                        <div className="item">
+                            排行榜
+                            <span className="closeBox">
+                            </span>
+                        </div>
+                        <div className="input">
+                            <input type = "text" />
+                            <div className="icon">
+                            </div>
                         </div>
                     </div>
-                    <div className="item">
-                        历史
-                    </div>
-                    <div>
-                        登录
-                    </div>
-                    <div>
-                        注册
+                    <div className="right">
+                        <div className="item">
+                            历史
+                        </div>
+                        <div>
+                            登录
+                        </div>
+                        <div>
+                            注册
+                        </div>
                     </div>
                 </div>
             </div>
