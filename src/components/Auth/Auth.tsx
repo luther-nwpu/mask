85a1a0194cd9cfa71c1b1a865ae5d4070b60f6ea
e <luthernpu@gmail.com>
2019-02-17 13:34:31 +0800

    d

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -8,22 +8,46 @@ export class Auth extends React.Component {
     public state = {
         tabNum: AuthTab.LOGIN
     }
+    public changeTab(tabNum: Number) {
+        this.setState({ tabNum })
+    }
     public render() {
         return (
             <div className="auth-component">
                 <div className="modal">
                     <div className="close"></div>
                     <div className="title">
-                        <div className={ this.state.tabNum === AuthTab.LOGIN ? 'item' : 'item_select'}>                        
+                        <div className={ this.state.tabNum === AuthTab.LOGIN ? 'item_select' : 'item'} onClick={() => this.changeTab(AuthTab.LOGIN)}>                        
                             登录
                         </div>
-                        <div className={this.state.tabNum === AuthTab.REGISTER ? 'item' : 'item_select'}>                        
+                        <div className={this.state.tabNum === AuthTab.REGISTER ? 'item_select' : 'item'} onClick={() => this.changeTab(AuthTab.REGISTER)}>                        
                             注册
                         </div>
                     </div>
                     <div className="content">
+                        
                         { this.state.tabNum === AuthTab.LOGIN ? (
                             <div>
+                                <div>
+                                    账号登录
+                                </div>
+                                <div>
+                                    <input />
+                                </div>
+                                <div>
+                                    <input />
+                                </div>
+                                <div>   
+                                    <div>
+                                        <input type="checkbox" />
+                                        一周内自动登录
+                                    </div>
+                                    <div>
+                                        找回密码
+                                    </div>
+                                </div>
+                                <div>
+                                </div>
                             </div>       
                         ) : (
                             <div>
