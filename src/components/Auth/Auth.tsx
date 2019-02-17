340f444e3e6c5363280fc389c6d83a7e2ef5cc77
e <luthernpu@gmail.com>
2019-02-17 09:30:55 +0800

    d

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react'
 import './Auth.scss'
+import { AuthTab } from '@config'
 export class Auth extends React.Component {
     constructor(props) {
         super(props)
+    }
+    public state = {
+        tabNum: AuthTab.LOGIN
     }
     public render() {
         return (
             <div className="auth-component">
                 <div className="modal">
-                    
+                    <div className="close">
+                    </div>
                 </div>
             </div>
         )
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,9 @@
 export enum Platform {
     HOME = 0,
-    ADMIN = 1
+    ADMIN = 1,
+}
+
+export enum AuthTab {
+    LOGIN = 0,
+    REGISTER = 1,
 }
