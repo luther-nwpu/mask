27025c2c04355a3cd8a96979cb1b42f7442b7a43
e <luthernpu@gmail.com>
2019-02-17 15:36:18 +0800

    d

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,15 +1,33 @@
 import * as React from 'react'
 import './Auth.scss'
 import { AuthTab } from '@config'
+import  mylogo from '@assets/mylogo.png'
 export class Auth extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            tabNum: AuthTab.LOGIN,
+            login: {
+                username: '',
+                password: ''
+            }
+        }
     }
     public state = {
-        tabNum: AuthTab.LOGIN
+        tabNum: AuthTab.LOGIN,
+        login: {
+            username: '',
+            password: ''
+        }
     }
     public changeTab(tabNum: Number) {
         this.setState({ tabNum })
+    }
+    public handleLoginUsername(event: any) {
+        this.setState({ login: { username: event.target.value }, ...this.state.login })
+    }
+    public handleLoginPassword(event: any) {
+        this.setState({ login: { password: event.target.value }, ...this.state.login })
     }
     public render() {
         return (
@@ -25,34 +43,59 @@ export class Auth extends React.Component {
                         </div>
                     </div>
                     <div className="content">
-                        
-                        { this.state.tabNum === AuthTab.LOGIN ? (
-                            <div>
+                        <div className="left">
+                            { this.state.tabNum === AuthTab.LOGIN ? (
+                                <div className="login">
+                                    <div className="item">
+                                        账号登录
+                                    </div>
+                                    <div className="item">
+                                        <input value={this.state.login.username} onChange={() => this.handleLoginUsername(event)} placeholder="请输入邮箱/手机/账号"/>
+                                    </div>
+                                    <div className="item">
+                                        <input value={this.state.login.password} onChange={() => this.handleLoginPassword(event)} placeholder="请输入密码"/>
+                                    </div>
+                                    <div className="item">   
+                                        <div className="judgelogin">
+                                            <input type="checkbox" />
+                                            <div> 一周内自动登录 </div>
+                                        </div>
+                                        <div className="switchFindPassword">
+                                            <a href="/findPassword">找回密码</a>
+                                        </div>
+                                    </div>
+                                    <div className="item">
+                                        <button> 登录 </button>
+                                    </div>
+                                </div>       
+                            ) : (
                                 <div>
-                                    账号登录
-                                </div>
-                                <div>
-                                    <input />
-                                </div>
-                                <div>
-                                    <input />
-                                </div>
-                                <div>   
                                     <div>
-                                        <input type="checkbox" />
-                                        一周内自动登录
+                                        <div> 手机/邮箱</div>
+                                        <input />
                                     </div>
                                     <div>
-                                        找回密码
+                                        <input />
+                                    </div>
+                                    <div>
+                                        <input />
+                                    </div>
+                                    <div>
+                                        <input /> 点击获取验证码
+                                    </div>
+                                    <div>
+                                        <input type="checkBox" />
+                                        你已经阅读用户协议书上。
+                                    </div>
+                                    <div>
+                                        注册
                                     </div>
                                 </div>
-                                <div>
-                                </div>
-                            </div>       
-                        ) : (
-                            <div>
-                            </div>
-                        )}
+                            )}
+                        </div>
+                        <div className="right">
+                            <img src= {mylogo} />
+                        </div>
                     </div>
                 </div>
             </div>
