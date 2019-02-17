10dd164076ac79895ecc39e0769e29ae3740246b
e <luthernpu@gmail.com>
2019-02-17 10:10:29 +0800

    d

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -12,7 +12,23 @@ export class Auth extends React.Component {
         return (
             <div className="auth-component">
                 <div className="modal">
-                    <div className="close">
+                    <div className="close"></div>
+                    <div className="title">
+                        <div className={ this.state.tabNum === AuthTab.LOGIN ? 'item' : 'item_select'}>                        
+                            登录
+                        </div>
+                        <div className={this.state.tabNum === AuthTab.REGISTER ? 'item' : 'item_select'}>                        
+                            注册
+                        </div>
+                    </div>
+                    <div className="content">
+                        { this.state.tabNum === AuthTab.LOGIN ? (
+                            <div>
+                            </div>       
+                        ) : (
+                            <div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
