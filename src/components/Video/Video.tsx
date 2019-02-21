185b7662943a73caf9474bc1a1238aadaade3762
e <luthernpu@gmail.com>
2019-02-21 18:26:54 +0800

    d

---
+++ src/components/Video/Video.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import './Video.scss'
+export class Video extends React.Component {
+    public render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
---
+++ src/pages/VideoPage/VideoPage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export class VideoPage extends React.Component {
+    public render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
