814cc97cb9e4a29ebcf03094c558d6e1c543dd4d
e <luthernpu@gmail.com>
2019-02-24 11:10:15 +0800

    d

---
+++ src/components/UploadVideoComponent/UploadVideoComponent.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import './UploadVideoComponent.scss'
+import uploadImg from '@assets/bg-upload-video.jpg'
+
+export class UploadVideoComponent extends React.Component {
+    constructor(props) {
+        super(props)
+        document.addEventListener('dragover', function(e) {
+            e.preventDefault()
+            e.stopPropagation()
+        })
+        document.addEventListener('dragenter', function(e) {
+            e.preventDefault()
+            e.stopPropagation()
+        })
+        document.addEventListener('drop', function(e) {
+            // 必须要禁用浏览器默认事件
+            e.preventDefault()
+            e.stopPropagation()
+            const files = e.dataTransfer.files
+            let reader = new FileReader()
+            reader.readAsText(files[0], 'utf-8')
+            reader.onload = function (evt) {
+                console.log(evt)
+            }
+        })
+    }
+    public uploadVideo() {
+        console.log('111')
+    }
+    public render() {
+        return (
+            <div id="dashboard" className="upload-video-component">
+                <img src={uploadImg} />
+                <div className="upload-video">
+                    上传视频
+                </div>
+                <div className="upload-text">
+                    上传节目到好嗨哟，即表示你已经阅读并且同意遵守好嗨哟的<a>视频上传用户协议 </a>
+                </div>
+            </div>
+        )
+    }
+}
--- src/pages/UploadVideo/UploadVideo.tsx
+++ src/pages/UploadVideo/UploadVideo.tsx
@@ -1,43 +1,17 @@
 import * as React from 'react'
 import './UploadVideo.scss'
-import uploadImg from '@assets/bg-upload-video.jpg'
 
 export class UploadVideo extends React.Component {
     constructor(props) {
         super(props)
-        document.addEventListener('dragover', function(e) {
-            e.preventDefault()
-            e.stopPropagation()
-        })
-        document.addEventListener('dragenter', function(e) {
-            e.preventDefault()
-            e.stopPropagation()
-        })
-        document.addEventListener('drop', function(e) {
-            // 必须要禁用浏览器默认事件
-            e.preventDefault()
-            e.stopPropagation()
-            const files = e.dataTransfer.files
-            let reader = new FileReader()
-            reader.readAsText(files[0], 'utf-8')
-            reader.onload = function (evt) {
-                console.log(evt)
-            }
-        })
     }
-    public uploadVideo() {
-        console.log('111')
+    public state = {
+        
     }
     public render() {
         return (
-            <div id="dashboard" className="upload-video-component">
-                <img src={uploadImg} />
-                <div className="upload-video">
-                    上传视频
-                </div>
-                <div className="upload-text">
-                    上传节目到好嗨哟，即表示你已经阅读并且同意遵守好嗨哟的<a>视频上传用户协议 </a>
-                </div>
+            <div className="upload-video-page">
+
             </div>
         )
     }
