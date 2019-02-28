1f7eb72dd2c1a06a76366cb43087fb2c691f2174
e <luthernpu@gmail.com>
2019-02-28 09:31:29 +0800

    d

--- src/models/Barrage.ts
+++ src/models/Barrage.ts
@@ -1,10 +1,7 @@
 export class Barrage {
-    private value: string;
-    private time: number;
-    private color: string;
-    public speed: number;
-    public fontSize: number;
-    constructor() {
-
-    }
+    private value: string
+    private time: number
+    private color: string
+    public speed: number
+    public fontSize: number
 }
