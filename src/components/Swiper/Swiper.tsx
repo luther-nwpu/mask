c7d0604b1322b8649e8fef9baf7cd31a1fe124c1
e <luthernpu@gmail.com>
2019-03-26 10:00:12 +0800

    d

--- src/components/Swiper/Swiper.tsx
+++ src/components/Swiper/Swiper.tsx
@@ -3,9 +3,14 @@ import './Swiper.scss'
 
 export class Swiper extends React.Component {
     public state = {
-        thumber: [
+        thumbers: [
             {
                 imgUrl: 'https://pub-static.haozhaopian.net/static/web/site/features/cn/crop/images/crop_20a7dc7fbd29d679b456fa0f77bd9525d.jpg',
+                title: '你好啊',
+                imghref: 'https://dsadad'
+            },
+            {
+                imgUrl: 'https://img0.sc115.com/uploads/sc/jpgs/05/xpic6813_sc115.com.jpg',
                 title: '你好啊',
                 imghref: 'https://dsadad'
             },
@@ -15,39 +20,70 @@ export class Swiper extends React.Component {
                 imghref: 'https://dsadad'
             },
             {
-                imgUrl: 'https://pub-static.haozhaopian.net/static/web/site/features/cn/crop/images/crop_20a7dc7fbd29d679b456fa0f77bd9525d.jpg',
-                title: '你好啊',
-                imghref: 'https://dsadad'
-            },
-            {
-                imgUrl: 'https://pub-static.haozhaopian.net/static/web/site/features/cn/crop/images/crop_20a7dc7fbd29d679b456fa0f77bd9525d.jpg',
+                imgUrl: 'http://img.mp.itc.cn/upload/20170602/73b39bdd3f664793b74e78fc9e244384_th.jpg',
                 title: '你好啊',
                 imghref: 'https://dsadad'
             }
-        ]
+        ],
+        left: 0,
+        timer: null
     }
+    constructor(props) {
+        super(props)
+        this.autoPlay()
+    }
+    public autoPlay = function() {
+        this.setState({    
+            timer: setInterval(() => {
+                this.nextPic()
+            },1000)
+        })
+    }
+    public nextPic = function() {
+        let newLeft
+        if(this.state.left === -600 * (this.state.thumbers.length - 1)){
+            newLeft = 0
+        }else{
+            newLeft = this.state.left - 600
+        }
+        this.setState({
+            left: newLeft
+        })
+    }
+    public prevPic = function() {
+        let newLeft
+        if(this.state.left === 0){
+            newLeft = -2400
+        }else{
+            newLeft = this.state.left + 600
+        }
+        this.setState({
+            left: newLeft
+        })
+    }
+
     public render() {
         return (
             <div className="swiper-component">
-                <div className="imgScroll">
-                    <div className="showContainer">
-                        <div className="container">
-                            <img src="https://dimg02.c-ctrip.com/images/100b11000000qezw729A4_R_1600_10000_Mtg_7.jpg" alt="A cat" />
-                            <img src="https://dimg05.c-ctrip.com/images/100u0x000000le38uA71D_R_1600_10000_Mtg_7.jpg" alt="A dog" />
-                            <img src="https://dimg08.c-ctrip.com/images/100811000000qrlfxA3E0_R_1600_10000_Mtg_7.jpg" alt="dandelion" />
-                        </div>
+                <div className="showContainer">
+                    <div className="container" style={{ left: `${this.state.left}px` }}>
+                        {
+                            this.state.thumbers.map((value) => {
+                                return (<div className="dd"> <img src={value.imgUrl} /> </div>)
+                            })
+                        }
                     </div>
-                    <div className="dots">
-                        <span className="dot active"></span>
-                        <span className="dot"></span>
-                        <span className="dot"></span>
-                    </div>
-                    <div className="left-triangle triangle">
-                        <span>&lt;</span>
-                    </div>
-                    <div className="right-triangle triangle">
-                        <span>&gt;</span>
-                    </div>
+                </div>
+                <div className="dots">
+                    <span className="dot active"></span>
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                </div>
+                <div className="left-triangle triangle">
+                    <span>&lt;</span>
+                </div>
+                <div className="right-triangle triangle">
+                    <span>&gt;</span>
                 </div>
             </div>
         )
