c40762d80a1c45f2b9615f9682d1ddcf84b4054f
e <luthernpu@gmail.com>
2019-05-15 13:06:07 +0800

    d

--- src/components/MySubscription/MySubscription.tsx
+++ src/components/MySubscription/MySubscription.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react'
 import './MySubscription.scss'
-import { TokenGet } from '@lib/helper'
+import { TokenGet, TokenPost } from '@lib/helper'
 import avator_default_jpg from '@assets/avator_default.jpg'
+import history from '@router'
+import empty_subscribe_svg from '@assets/subscribe_btn_3.svg'
 export class MySubscription extends React.Component {
     constructor(props: any) {
         super(props)
@@ -24,14 +26,53 @@ export class MySubscription extends React.Component {
         subscribeUsers: []
     }
 
+    public handleUnSubscribe(e, id) {
+        e.stopPropagation()
+        TokenPost('/subscribe/unSubscribeUser', {
+            subscriberId: id
+        }).then((res) => {
+            if(res.success) {
+                this.setState({
+                    subscribeUsers: res.result
+                })
+            } else {
+                alert('请求出错')
+            }
+        })
+    }
+
+    public switchToUser(userId) {
+        history.push(`/haiyouUser/${userId}`)
+    }
+
     public render() {
         return (
             <div className="mysubscription-component">
                 {
-                    this.state.subscribeUsers.map((value) => {
+                    this.state.subscribeUsers.length == 0 ? (
+                        <div className="empty-subscribe">
+                            <img src={empty_subscribe_svg} />
+                            <span className="text"> 您当前并没有订阅用户。 </span>
+                        </div>
+                    ) : ''
+                }
+                {
+                    this.state.subscribeUsers.map((value, key) => {
                         return (
-                            <div className="subscribe-item">
-                                <img src={ avator_default_jpg } /> 
+                            <div key = { key } className="subscribe-item" onClick={ () => this.switchToUser(value.suser_id) }>
+                                <img src={ avator_default_jpg } />
+                                <div className="item-description">
+                                    <div className="username">
+                                        { value.suser.username }
+                                    </div>
+                                    <div className="signature">
+                                        { value.suser.signature || '您当前并没有签名' }
+                                    </div>
+                                    <div className="item-bottom">
+                                        订阅者： { value.subscribeCount }
+                                        <span onClick={(e) => this.handleUnSubscribe(e, value.id)} className="unsubscribe-btn"> 取消订阅 </span>
+                                    </div>
+                                </div>
                             </div>
                         )
                     })
