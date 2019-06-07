43621a45ac2bb4f92da3b7264c169153409a6e83
e <luthernpu@gmail.com>
2019-06-07 11:44:40 +0800

    d

--- src/components/MyVideo/MyVideo.tsx
+++ src/components/MyVideo/MyVideo.tsx
@@ -65,7 +65,7 @@ export class MyVideo extends React.Component {
                     this.state.videos.map((value, key) => {
                         return (
                             <div className="item" key={key} onClick={() => this.switchToLink(`/edithaiyou/${value&&value.id}`)}>
-                                <img src={value.picture && value['picture']['url'] || draft_default_png } className="avator" />
+                                <img src={(value['picture'] && value['picture']['url']) } className="avator" />
                                 <div className="right-description">
                                     <div className="first"> 
                                         <div className="category"> { value.category } </div>
